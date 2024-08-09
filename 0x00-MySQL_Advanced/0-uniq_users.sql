59bc246e578a14aaf390eac11c89bccc075aaddf
Okoyen E. Precious <x17.ebi@icloud.com>
2024-08-09 02:42:56 +0100

    Added task 0-13

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- An SQL Script that creates a `user` table
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
